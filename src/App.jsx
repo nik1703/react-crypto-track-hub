bf63f50f8b90008dec2a7a55dc0aed98b1de7a4e
Nik <niklasmoog@gmail.com>
2024-03-06 18:52:09 +0100

    createBrowserRouter in

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
 
 const App = () => {
-
-
 	return (
 		<>
-			<div>
-				App
-			</div>
+			<Header />
+			<main className="bg-[#0F111A]">
+				<Outlet />
+			</main>
 		</>
 	);
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css';
+import './index.scss';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HomePage, NewsPage, FavoritesPage, NotFoundPage } from './pages/';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+		errorElement: <NotFoundPage />,
+		children: [
+			{
+				path: '/',
+				element: <HomePage />
+			},
+			{
+				path: '/news',
+				element: <NewsPage />
+			},
+			{
+				path: '/favorites',
+				element: <FavoritesPage />
+			}
+		]
+	}
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+	<RouterProvider router={router} />
 );
